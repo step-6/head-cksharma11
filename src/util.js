012086658e355dba4e02d93ab69910b531306121
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2018-12-04 18:47:55 +0530

    Add parseInput():
    Supports only '-n' and default numOfLines i.e 10

--- src/util.js
+++ src/util.js
@@ -2,6 +2,13 @@ const applyFunction = function(callback, arg1, arg2){
   return callback(arg1, arg2);
 }
 
+const parseInputs = function(...fileNames){
+  let parsedInputs = {option : 'n', numOfLines : 10};
+  parsedInputs.fileNames = fileNames;
+  return parsedInputs;
+}
+
 module.exports = {
-  applyFunction
+  applyFunction,
+  parseInputs
 }
--- test/utilTest.js
+++ test/utilTest.js
@@ -1,6 +1,7 @@
 const { equal, deepEqual } = require('assert');
 const {
-  applyFunction
+  applyFunction,
+  parseInputs
 } = require('../src/util.js');
 
 const add = (a, b) => a+b;
@@ -8,5 +9,15 @@ const add = (a, b) => a+b;
 describe('applyFunction', function(){
   it('should call given function with arg1 and arg2', function(){
     equal(applyFunction(add, 5, 5), 10);
+  });
+});
+
+describe('parseInputs', function(){
+  it('should return empty array in fileNames key when no file name given', function(){
+    deepEqual(parseInputs(),{ option: 'n', numOfLines: 10, fileNames: [] });
+  });
+
+  it('should insert given file names in fileNames key', function(){
+    deepEqual(parseInputs('file1', 'file2'),{ option: 'n', numOfLines: 10, fileNames: ['file1', 'file2'] });
   });
 });

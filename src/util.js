9d7d2254c5abea12ec9c83d533d8c0f199ac7884
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2018-12-05 23:03:46 +0530

    Refactor parseInputs().

--- src/util.js
+++ src/util.js
@@ -1,27 +1,30 @@
 const parseInputs = function(inputs){
-  let parsedInputs = {option : '-n', value : 10, fileNames : inputs};
   let isStartsWithHyphen = inputs[0].startsWith('-');
 
   if(isStartsWithHyphen && isFinite(inputs[0][1])){
-    parsedInputs.value = inputs[0].slice(1);
-    parsedInputs.fileNames = inputs.slice(1)
-    return parsedInputs;
+    return {
+      option: '-n', 
+      value: inputs[0].slice(1), 
+      fileNames: inputs.slice(1)
+    }
   }
 
   if(isStartsWithHyphen && inputs[0].length > 2){
-    parsedInputs.option = inputs[0].slice(0,2);
-    parsedInputs.value = inputs[0].substr(2);
-    parsedInputs.fileNames = inputs.slice(1);
-    return parsedInputs;
+    return {
+      option: inputs[0].slice(0,2), 
+      value: inputs[0].substr(2), 
+      fileNames: inputs.slice(1)
+    }
   }
-
+  
   if(isStartsWithHyphen && inputs[0].length == 2){
-    parsedInputs.value = inputs[1];
-    parsedInputs.option = inputs[0].slice(0,2);
-    parsedInputs.fileNames = inputs.slice(2);
+     return {
+      option: inputs[0].slice(0,2), 
+      value: inputs[1], 
+      fileNames: inputs.slice(2)
+    }
   }
-
-  return parsedInputs;
+  return {option : '-n', value : 10, fileNames : inputs};
 }
 
 module.exports = {

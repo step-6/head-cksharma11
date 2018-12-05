aaa8fd498b5853a193cad7ed971759ef741087a6
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2018-12-05 11:33:21 +0530

    Add more test cases for parseInputs()

--- test/utilTest.js
+++ test/utilTest.js
@@ -4,8 +4,18 @@ const {
 } = require('../src/util.js');
 
 describe('parseInputs', function(){
-  it('should categorize head inptes and return object', function(){
+  it('should categorize head inptes and return object with option as -n', function(){
     let userArgs = ['-n5', 'file1'];
     deepEqual(parseInputs(userArgs),{ option: '-n', value: 5, fileNames: ['file1'] });
+  });
+  
+  it('should categorize head inptes and return object with option as -c', function(){
+    let userArgs = ['-c5', 'file1'];
+    deepEqual(parseInputs(userArgs),{ option: '-c', value: 5, fileNames: ['file1'] });
+  });
+  
+  it('should return default values', function(){
+    let userArgs = ['file1'];
+    deepEqual(parseInputs(userArgs),{ option: '-n', value: 10, fileNames: ['file1'] });
   });
 });

0c87c22dafaf904ff5a7b0d940d0d8cf45a8a0af
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2018-12-05 16:33:06 +0530

    Refactor parseInputs(): add isStartsWithHyphen condition

--- src/util.js
+++ src/util.js
@@ -1,19 +1,20 @@
 const parseInputs = function(inputs){
   let parsedInputs = {option : '-n', value : 10, fileNames : inputs};
+  let isStartsWithHyphen = inputs[0].startsWith('-');
 
-  if(inputs[0].startsWith('-') && isFinite(inputs[0][1])){
+  if(isStartsWithHyphen && isFinite(inputs[0][1])){
     parsedInputs.value = inputs[0].slice(1);
     parsedInputs.fileNames = inputs.slice(1)
     return parsedInputs;
   }
 
-  if(inputs[0].startsWith('-') && inputs[0].length > 2){
+  if(isStartsWithHyphen && inputs[0].length > 2){
     parsedInputs.option = inputs[0].slice(0,2);
     parsedInputs.value = inputs[0].substr(2);
     parsedInputs.fileNames = inputs.slice(1);
   }
 
-  if(inputs[0].startsWith('-') && inputs[0].length == 2){
+  if(isStartsWithHyphen && inputs[0].length == 2){
     parsedInputs.value = inputs[1];
     parsedInputs.option = inputs[0].slice(0,2);
     parsedInputs.fileNames = inputs.slice(2);

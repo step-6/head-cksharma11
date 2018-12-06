6c5a4715a626d6048c03dbf09a32e00fd3053f04
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2018-12-06 09:29:04 +0530

    Remove comments from main().

--- head.js
+++ head.js
@@ -11,22 +11,6 @@ const {
   head
 } = require('./src/lib.js');
 
-/* 
-  Usage:
-  node ./head.js file1
-  node ./head.js -n5 file1
-  node ./head.js -n 5 file1
-  node ./head.js -5 file1
-  node ./head.js file1 file2
-  node ./head.js -n 5 file1 file2
-  node ./head.js -n5 file1 file2
-  node ./head.js -5 file1 file2 
-  node ./head.js -c5 file1
-  node ./head.js -c 5 file1
-  node ./head.js -c5 file1 file2
-  node ./head.js -c 5 file1 file2
-*/
-
 const main = function(){
   let parsedInputs = parseInputs(process.argv.slice(2));
   let errorLog = checkErrors(parsedInputs);

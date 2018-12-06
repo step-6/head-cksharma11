3c04c1b61b44b838ff7809e879307e33737e22d4
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2018-12-06 16:50:50 +0530

    Rename readFileSync() and existsSync() to identity and isExists

--- test/libTest.js
+++ test/libTest.js
@@ -131,31 +131,34 @@ describe('fileNotFoundLog', function(){
 });
 
 describe('generateHeadResult', function(){
-  let readFileSync = (x) => x;
-  let existsSync = (x) => true;
+  let identity = (x) => x;
+  let isExists = (x) => true;
   let TestFile = "Line 1\n"+
                  "Line 2\n"+
                  "Line 3";
+  let TestFile2 ="Line One\n"+
+                 "Line Two\n"+
+                 "Line Three";
   it('should return error when option value is 0', function(){
     let expectedOut = 'head: illegal line count -- 0' 
-    equal(generateHeadResult(readFileSync, existsSync, {option:'-n', value:0, fileNames: ['file1']}), expectedOut);
+    equal(generateHeadResult(identity, isExists, {option:'-n', value:0, fileNames: ['file1']}), expectedOut);
   });
   
   it('should return error when option negative value passed', function(){
     let expectedOut = 'head: illegal line count -- -1' 
-    equal(generateHeadResult(readFileSync, existsSync, {option:'-n', value:-1, fileNames: ['file1']}), expectedOut);
+    equal(generateHeadResult(identity, isExists, {option:'-n', value:-1, fileNames: ['file1']}), expectedOut);
   });
   
   it('should return error when length of fileNames is 0', function(){
     let expectedOut = 'head: option requires an argument -- n\n'+
                       'usage: head [-n lines | -c bytes] [file ...]' 
-    equal(generateHeadResult(readFileSync, existsSync, {option:'-n', value:1, fileNames: []}), expectedOut);
+    equal(generateHeadResult(identity, isExists, {option:'-n', value:1, fileNames: []}), expectedOut);
   });
 
   it('should return n number of lines when single file is passed', function(){
     let expectedOut = "Line 1\n"+
                       "Line 2";
     
-    equal(generateHeadResult(readFileSync, existsSync, {option:'-n', value:2, fileNames:[TestFile]}), expectedOut);
+    equal(generateHeadResult(identity, isExists, {option:'-n', value:2, fileNames:[TestFile]}), expectedOut);
   });
 });

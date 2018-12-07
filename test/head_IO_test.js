0bea8aca922687e6b4caea6ff0782986853740de
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2018-12-07 14:29:58 +0530

    Change variable modifier let to const in head_IO_test.js

--- test/head_IO_test.js
+++ test/head_IO_test.js
@@ -5,37 +5,37 @@ const {
 
 describe('parseInputs', function(){
   it('should categorize head inptes and return object with option as -n', function(){
-    let userArgs = ['-n5', 'file1'];
+    const userArgs = ['-n5', 'file1'];
     deepEqual(parseInputs(userArgs),{ option: '-n', value: 5, fileNames: ['file1'] });
   });
   
   it('should categorize head inptes and return object with option as -c', function(){
-    let userArgs = ['-c5', 'file1'];
+    const userArgs = ['-c5', 'file1'];
     deepEqual(parseInputs(userArgs),{ option: '-c', value: 5, fileNames: ['file1'] });
   });
   
   it('should return default values', function(){
-    let userArgs = ['file1'];
+    const userArgs = ['file1'];
     deepEqual(parseInputs(userArgs),{ option: '-n', value: 10, fileNames: ['file1'] });
   });
   
   it('should categorize head inptes without passing option i.e. -11', function(){
-    let userArgs = ['-11', 'file1'];
+    const userArgs = ['-11', 'file1'];
     deepEqual(parseInputs(userArgs),{ option: '-n', value: 11, fileNames: ['file1'] });
   });
   
   it('should categorize head inptes with case -n 5', function(){
-    let userArgs = ['-n', '5', 'file1'];
+    const userArgs = ['-n', '5', 'file1'];
     deepEqual(parseInputs(userArgs),{ option: '-n', value: 5, fileNames: ['file1'] });
   });
 
   it('should categorize head inptes and return object with invalid option', function(){
-    let userArgs = ['-v5', 'file1'];
+    const userArgs = ['-v5', 'file1'];
     deepEqual(parseInputs(userArgs),{ option: '-v', value: 5, fileNames: ['file1'] });
   });
   
   it('should categorize head inptes and return object with string as value', function(){
-    let userArgs = ['-n', 'five', 'file1'];
+    const userArgs = ['-n', 'five', 'file1'];
     deepEqual(parseInputs(userArgs),{ option: '-n', value: 'five', fileNames: ['file1'] });
   });
 });

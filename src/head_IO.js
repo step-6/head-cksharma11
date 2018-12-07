022a7f34963bb4191b2df4e81292baaeea25a0d9
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2018-12-07 15:26:12 +0530

    Refactor parseInputs():
    Extract :
      --> getNumberOptions()
      --> getOptionsWithCount()
      --> getOptionsWithoutCount()

--- src/head_IO.js
+++ src/head_IO.js
@@ -1,39 +1,56 @@
+const isOptionSpecified = function(input) {
+  return input.startsWith("-");
+};
+
+const isNumberOption = function(input) {
+  return isOptionSpecified(input) && isFinite(input[1]);
+};
+
+const isOptionWithCount = function(input) {
+  return isOptionSpecified(input) && input.length > 2;
+};
+
+const isOptionWithoutCount = function(input) {
+  return isOptionSpecified(input) && input.length == 2;
+};
+
+const getNumberOptions = function(inputs) {
+  return {
+    option: "-n",
+    value: inputs[0].slice(1),
+    fileNames: inputs.slice(1)
+  };
+};
+
+const getOptionsWithCount = function(inputs) {
+  return {
+    option: inputs[0].slice(0, 2),
+    value: inputs[0].substr(2),
+    fileNames: inputs.slice(1)
+  };
+};
+
+const getOptionsWithoutCount = function(inputs) {
+  return {
+    option: inputs[0].slice(0, 2),
+    value: inputs[1],
+    fileNames: inputs.slice(2)
+  };
+};
+
 const parseInputs = function(inputs) {
-  const isOptionSpecified = input => input.startsWith("-");
-
-  const isNumberOption = input =>
-    isOptionSpecified(input) && isFinite(input[1]);
-
-  const isOptionWithCount = input =>
-    isOptionSpecified(input) && input.length > 2;
-
-  const isOptionWithoutCount = input =>
-    isOptionSpecified(input) && input.length == 2;
-
   const defaultOptions = { option: "-n", value: 10, fileNames: inputs };
 
   if (isNumberOption(inputs[0])) {
-    return {
-      option: "-n",
-      value: inputs[0].slice(1),
-      fileNames: inputs.slice(1)
-    };
+    return getNumberOptions(inputs);
   }
 
   if (isOptionWithCount(inputs[0])) {
-    return {
-      option: inputs[0].slice(0, 2),
-      value: inputs[0].substr(2),
-      fileNames: inputs.slice(1)
-    };
+    return getOptionsWithCount(inputs);
   }
 
   if (isOptionWithoutCount(inputs[0])) {
-    return {
-      option: inputs[0].slice(0, 2),
-      value: inputs[1],
-      fileNames: inputs.slice(2)
-    };
+    return getOptionsWithoutCount(inputs);
   }
 
   return defaultOptions;

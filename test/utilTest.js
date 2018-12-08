e4c73edae6b05d6361b3b25e3611c16395fed3ef
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2018-12-08 10:48:01 +0530

    Separate test cases for zip().

--- test/utilTest.js
+++ test/utilTest.js
@@ -2,10 +2,12 @@ const { deepEqual } = require('assert');
 const { zip } = require('../src/util.js');
 
 describe('zip', function(){
-  it('should zip two arrays', function(){
+  it('should zip two arrays of same length', function(){
     deepEqual(zip([1],[1]),[[1,1]]);
     deepEqual(zip([1,2,3],[4,5,6]),[[1,4],[2,5],[3,6]]);
+  });
 
+  it('should zip arrays with different lengths', function(){
     deepEqual(zip([1,2,3],[4,5,6,11,2]),[[1,4],[2,5],[3,6]]);
     deepEqual(zip([1,2,3,1,2,3],[4,5,6]),[[1,4],[2,5],[3,6]]);
   });

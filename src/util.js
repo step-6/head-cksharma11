0dca3b7f37b4db65b5a393f05851ebba6ae12af3
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2018-12-09 11:15:50 +0530

    Use camelcase in zip().

--- src/util.js
+++ src/util.js
@@ -1,8 +1,8 @@
-const zip = function(list_1, list_2){
-  let maxLength = Math.min(list_1.length, list_2.length);
+const zip = function(list1, list2){
+  let maxLength = Math.min(list1.length, list2.length);
   let zippedList = [];
   for(let index = 0; index < maxLength; index++){
-    zippedList[index] = [list_1[index],list_2[index]];
+    zippedList[index] = [list1[index],list2[index]];
   };
   return zippedList;
 };

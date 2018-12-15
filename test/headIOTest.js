cf463898b7112c2d564a964901a41c8c8c0642e7
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2018-12-16 00:04:09 +0530

    Reformat headIOTest

--- test/headIOTest.js
+++ test/headIOTest.js
@@ -5,65 +5,79 @@ describe("parseInputs", function() {
   describe("should categorize inputs and return object", function() {
     it("with option as -n", function() {
       const userArgs = ["-n5", "file1"];
-      assert.deepEqual(parseInputs(userArgs), {
+      const expectedOutput = {
         option: "-n",
         value: 5,
         fileNames: ["file1"]
-      });
+      };
+
+      assert.deepEqual(parseInputs(userArgs), expectedOutput);
     });
 
     it("with option as -c", function() {
       const userArgs = ["-c5", "file1"];
-      assert.deepEqual(parseInputs(userArgs), {
+      const expectedOutput = {
         option: "-c",
         value: 5,
         fileNames: ["file1"]
-      });
+      };
+
+      assert.deepEqual(parseInputs(userArgs), expectedOutput);
     });
 
     it("with default values", function() {
       const userArgs = ["file1"];
-      assert.deepEqual(parseInputs(userArgs), {
+      const expectedOutput = {
         option: "-n",
         value: 10,
         fileNames: ["file1"]
-      });
+      };
+
+      assert.deepEqual(parseInputs(userArgs), expectedOutput);
     });
 
     it("without passing option i.e. -11", function() {
       const userArgs = ["-11", "file1"];
-      assert.deepEqual(parseInputs(userArgs), {
+      const expectedOutput = {
         option: "-n",
         value: 11,
         fileNames: ["file1"]
-      });
+      };
+
+      assert.deepEqual(parseInputs(userArgs), expectedOutput);
     });
 
     it("with case -n 5", function() {
       const userArgs = ["-n", "5", "file1"];
-      assert.deepEqual(parseInputs(userArgs), {
+      const expectedOutput = {
         option: "-n",
         value: 5,
         fileNames: ["file1"]
-      });
+      };
+
+      assert.deepEqual(parseInputs(userArgs), expectedOutput);
     });
 
     it("with invalid option", function() {
       const userArgs = ["-v5", "file1"];
-      assert.deepEqual(parseInputs(userArgs), {
+      const expectedOutput = {
         option: "-v",
         value: 5,
         fileNames: ["file1"]
-      });
+      };
+
+      assert.deepEqual(parseInputs(userArgs), expectedOutput);
     });
 
     it("with string as value", function() {
       const userArgs = ["-n", "five", "file1"];
-      assert.deepEqual(parseInputs(userArgs), {
+      const expectedOutput = {
         option: "-n",
         value: "five",
         fileNames: ["file1"]
-      });
+      };
+
+      assert.deepEqual(parseInputs(userArgs), expectedOutput);
     });
   });
 });

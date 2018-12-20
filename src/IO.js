dcbf674df91ab4d2b825776ce23c10525aa8c557
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2018-12-20 14:40:59 +0530

    Change order of functions in IO.js

--- src/IO.js
+++ src/IO.js
@@ -1,3 +1,29 @@
+const parseInputs = function(inputs) {
+  const option = inputs[0];
+
+  if (isNumberOption(option)) {
+    return createParsedInputs("-n", inputs[0].slice(1), inputs.slice(1));
+  }
+
+  if (isOptionWithCount(option)) {
+    return createParsedInputs(
+      inputs[0].slice(0, 2),
+      inputs[0].substr(2),
+      inputs.slice(1)
+    );
+  }
+
+  if (isOptionWithoutCount(option)) {
+    return createParsedInputs(
+      inputs[0].slice(0, 2),
+      inputs[1],
+      inputs.slice(2)
+    );
+  }
+
+  return getDefaultOptions(inputs);
+};
+
 const isNumber = function(numberCandidate) {
   return !isNaN(numberCandidate);
 };
@@ -24,32 +50,6 @@ const createParsedInputs = function(option, count, fileNames) {
 
 const getDefaultOptions = function(inputs) {
   return { option: "-n", count: 10, fileNames: inputs };
-};
-
-const parseInputs = function(inputs) {
-  const option = inputs[0];
-
-  if (isNumberOption(option)) {
-    return createParsedInputs("-n", inputs[0].slice(1), inputs.slice(1));
-  }
-
-  if (isOptionWithCount(option)) {
-    return createParsedInputs(
-      inputs[0].slice(0, 2),
-      inputs[0].substr(2),
-      inputs.slice(1)
-    );
-  }
-
-  if (isOptionWithoutCount(option)) {
-    return createParsedInputs(
-      inputs[0].slice(0, 2),
-      inputs[1],
-      inputs.slice(2)
-    );
-  }
-
-  return getDefaultOptions(inputs);
 };
 
 module.exports = {
